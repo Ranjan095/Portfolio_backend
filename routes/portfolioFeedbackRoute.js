07679201950b6c74fe3a055b84b5d7161895ff13
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-08-28 15:29:03 +0530

    bug fixed (Time zoone bug)

--- routes/portfolioFeedbackRoute.js
+++ routes/portfolioFeedbackRoute.js
@@ -6,9 +6,24 @@ const { UserModel } = require("../models/portfolioFeedbackModel");
 let portfolioFeedbackRoute = express.Router();
 
 portfolioFeedbackRoute.post("/message/create", async (req, res) => {
+
+// // Create a Date object
+// const now = new Date();
+
+// // IST is UTC+5:30
+
+const now = new Date();
+
+// Create a new Date object with the UTC time equivalent to IST
+const istTime = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours() + 5, now.getUTCMinutes() + 30, now.getUTCSeconds());
+
+// Format the IST time as a string
+const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+const createdAt =await istTime.toLocaleString("en-IN", options);
+
   try {
     let { name, email, message } = req.body;
-    let createdAt = new Date();
+    // let createdAt = new Date();
     let newFeedBack = await new UserModel({ name, email, message, createdAt });
     await newFeedBack.save();
     res.status(200).send({ msg: "Feedback has been added" });
